9be1c0962b14a83912fa7437581941adb91201c2
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2019-11-21 16:39:11 +0000

    ...

--- test/test_Design.jl
+++ test/test_Design.jl
@@ -12,7 +12,7 @@ ts  = get_optimal_design(prior, p0, α, β)
 gs  = get_optimal_design(prior, p0, α, β; group_sequential = true)
 os  = get_optimal_design(prior, p0, α, β; one_stage = true)
 os2 = DesignIPModel(prior, p0, α, β;
-        nmax = 52, n1min = 52, n1max = 52,
+        nmax = 32, n1min = 32, n1max = 32,
         one_stage = true
     ) +
     minimal_expected_power(.8) +
