faeece92c53ac4dc20606346eb882ddafb817272
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2019-12-04 12:45:13 +0000

    fixed rb estimator

--- src/estimators/RaoBlackwellEstimator.jl
+++ src/estimators/RaoBlackwellEstimator.jl
@@ -6,7 +6,7 @@ string(estimator::RaoBlackwellEstimator) = "RaoBlackwellEstimator"
 function (estimator::RaoBlackwellEstimator)(x1::TI, x2::TI, design::TD) where {TI<:Integer,TD<:AbstractDesign}
     x = x1 + x2
     xx1   = 0:n1(design)
-    xx1   = xx1[n2.(design, xx1) .== n2(design, 1)]
+    xx1   = xx1[n2.(design, xx1) .== n2(design, x1)]
     nom   = sum( binomial.(n1(design) .- 1, xx1 .- 1) .* binomial.(n2.(design, xx1), x .- xx1) )
     denom = sum( binomial.(n1(design), xx1) .* binomial.(n2.(design, xx1), x .- xx1) )
     return nom / denom
--- test/test_RaoBlackwellEstimator.jl
+++ test/test_RaoBlackwellEstimator.jl
@@ -1,6 +1,9 @@
-p0, α    = .2, .05
-prior, β = PointMass(0.4), .1
-design   = Problem(
+using Test
+
+p0     = .2
+α, β   = .05, .2
+prior  = PointMass(0.4)
+design = Problem(
         minimise_expected_sample_size(prior),
         maximal_type_one_error_rate(p0, α),
         minimal_expected_power(prior, p0 + .05, 1 - β),
@@ -13,3 +16,9 @@ x1_early_stop = 0:n1(design)
 x1_early_stop = x1_early_stop[n2.(design, x1_early_stop) .== 0]
 
 @test all( mle.(x1_early_stop, 0, design) .== rbe.(x1_early_stop, 0, design) )
+
+XX = sample_space(design)
+
+@test all(0 .<= rbe.(XX[:,1], XX[:,2], design) .<= 1)
+
+@test bias.(0:.01:1, rbe, design) |> b -> abs.(b) |> maximum < 1e-12
