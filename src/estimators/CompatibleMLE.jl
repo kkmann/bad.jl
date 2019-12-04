5230f16c6b2fb2b60815e598c90ae7d4126314cf
Kevin Kunzmann <kevin.kunzmann@mrc-bsu.cam.ac.uk>
2019-12-04 21:24:14 +0000

    ...

--- src/estimators/CompatibleMLE.jl
+++ src/estimators/CompatibleMLE.jl
@@ -15,7 +15,7 @@ function (estimator::CompatibleMLE)(x1::TI, x2::TI, design::TD) where {TI<:Integ
     error("(x1,x2) not found in sample space, valid observation?")
 end
 
-function CompatibleMLE(design::TD; ϵ = 1e-4, b = 1., max_iter = 10^5) where {TD<:AbstractDesign}
+function CompatibleMLE(design::TD; λ = 1e0, ϵ = 1e-4, smoothmax_scale = 10., max_iter = 10^5) where {TD<:AbstractDesign}
 
     XX = sample_space(design)
     # precompute test decisions and get indices for rejection/non-rejection
@@ -28,19 +28,21 @@ function CompatibleMLE(design::TD; ϵ = 1e-4, b = 1., max_iter = 10^5) where {TD
     mles = mle.(XX[:, 1], XX[:, 2], design)
     nn   = size(XX, 1)
     # define versions for automatic differentiation, ll = loglikelihood
-    dbinom(x, n, p)  = gamma(n + 1)/gamma(x + 1)/gamma(n - x + 1)*(1 - p)^(n - x)*p^x
-    likelihood(p, i) = dbinom(XX[i,1], n1(design), p) * dbinom(XX[i,2], n2(design,XX[i,1]), p)
-    smoothmax(x, b)  = sum( x .* exp.(b.*x) ./ sum(exp.(b.*x)) )
+    dbinom(x, n, p)       = gamma(n + 1)/gamma(x + 1)/gamma(n - x + 1)*(1 - p)^(n - x)*p^x
+    likelihood(p, x1, x2) = dbinom(x1, n1(design), p) * dbinom(x2, n2(design, x1), p)
+    llresidual(p1, p2, i) = likelihood(p1, XX[i,1], XX[i,2]) - likelihood(p2, XX[i,1], XX[i,2])
+    smoothmax(x, scale)   = sum( x .* exp.(scale .* x) ./ sum(exp.(scale .* x)) )
     # objective
-    f(x...) = smoothmax([
-        ( likelihood(x[i], i) - likelihood(mles[i], i) )^2
-        for i in 1:length(x)
-    ], b)
+    function f(phat...)
+        [λ*(phat[i] - mles[i])^2 + llresidual(phat[i], mles[i], i)^2 for i in 1:length(phat)] |>
+        residuals -> smoothmax(residuals, smoothmax_scale)
+    end
     # set up JuMP model
     m = Model(
         with_optimizer(Ipopt.Optimizer,
-            max_iter        = max_iter,
-            constr_viol_tol = ϵ/10
+            max_iter                         = max_iter,
+            constr_viol_tol                  = ϵ/10,
+            acceptable_constr_viol_tol       = ϵ/10
         )
     )
     # define all variables upfront, start with estimates
@@ -71,11 +73,9 @@ function CompatibleMLE(design::TD; ϵ = 1e-4, b = 1., max_iter = 10^5) where {TD
     end
     # define objective
     register(m, :f, nn, f, autodiff = true)
-    # @NLobjective(m, Min, f(phat...))
-    @objective(m, Min, sum( (phat .- mles).^2 ) )
+    @NLobjective(m, Min, f(phat...) )
     optimize!(m)
     estimates = value.(phat)
-
     # build object and return
     return CompatibleMLE{eltype(XX),eltype(estimates),typeof(design)}(
         design, XX, estimates
--- test/test_CompatibleMLE.jl
+++ test/test_CompatibleMLE.jl
@@ -22,36 +22,51 @@ XX    = sample_space(design)
 resid = cmle.(XX[:,1], XX[:,2], design) .- mle.(XX[:,1], XX[:,2], design)
 @test .001 < maximum(resid)
 
-p0     = .2
-α, β   = .05, .2
-prior  = PointMass(0.4)
-design = Problem(
-        minimise_expected_sample_size(prior),
-        maximal_type_one_error_rate(p0, α),
-        minimal_expected_power(prior, p0 + .05, 1 - β),
-) |> optimise
 
-mle  = MaximumLikelihoodEstimator()
-@test compatible(mle, design, p0, α)[1]
 
-cmle = CompatibleMLE(design)
-@test compatible(cmle, design, p0, α)[1]
+α, β = .05, .2
+design_h0 = (p0, p1) -> Problem(
+                minimise_expected_sample_size(PointMass(p0)),
+                maximal_type_one_error_rate(p0, α),
+                minimal_expected_power(PointMass(p1), p0, 1 - β),
+        ) |> optimise
+design_h1 = (p0, p1) -> Problem(
+                minimise_expected_sample_size(PointMass(p1)),
+                maximal_type_one_error_rate(p0, α),
+                minimal_expected_power(PointMass(p1), p0, 1 - β),
+        ) |> optimise
 
-XX    = sample_space(design)
-resid = cmle.(XX[:,1], XX[:,2], design) .- mle.(XX[:,1], XX[:,2], design)
-@test maximum(resid) < 1e-4
+function test_design(design, p0)
+        mle  = MaximumLikelihoodEstimator()
+        @test compatible(mle, design, p0, α)[1]
+        cmle = CompatibleMLE(design, λ = 1.)
+        @test compatible(cmle, design, p0, α)[1]
+        XX    = sample_space(design)
+        resid = cmle.(XX[:,1], XX[:,2], design) .- mle.(XX[:,1], XX[:,2], design)
+        println(p0)
+        @test maximum(resid) < 1e-4
+end
 
-p = PValue(EstimatorOrdering(cmle), design, p0)
+for p0 = 0.1:.1:.7
+        p1 = p0 .+ .2
+        test_design(design_h0(p0, p1), p0)
+        test_design(design_h1(p0, p1), p0)
+end
 
-XX_ordered = p.ordered_sample_space
-@test all( bad.more_extreme.(
-                XX_ordered[2:end,1], XX_ordered[2:end,2],
-                XX_ordered[1:(end - 1),1], XX_ordered[1:(end - 1),2],
-                cmle,
-                design
-        ) )
+design  = design_h0(.4, .6)
+cmle    = CompatibleMLE(design, λ = 1)
 
-@test 1e-9 > maximum(abs.(p.(XX[:,1], XX[:,2]) - p_value.(XX[:,1], XX[:,2], p0, cmle, design) ))
+# p = PValue(EstimatorOrdering(cmle), design, p0)
 
-@time p.(XX[:,1], XX[:,2])
-@time p_value.(XX[:,1], XX[:,2], p0, EstimatorOrdering(cmle), design)
+# XX_ordered = p.ordered_sample_space
+# @test all( bad.more_extreme.(
+#                 XX_ordered[2:end,1], XX_ordered[2:end,2],
+#                 XX_ordered[1:(end - 1),1], XX_ordered[1:(end - 1),2],
+#                 cmle,
+#                 design
+#         ) )
+#
+# @test 1e-9 > maximum(abs.(p.(XX[:,1], XX[:,2]) - p_value.(XX[:,1], XX[:,2], p0, cmle, design) ))
+#
+# @time p.(XX[:,1], XX[:,2])
+# @time p_value.(XX[:,1], XX[:,2], p0, EstimatorOrdering(cmle), design)
